b38ac514a7bf5ab66c394bf85e7f0bc4e1028ec2
zhengwei <ow.zhengwei@gmail.com>
2025-02-01 19:44:18 +0800

    did up some ui

---
+++ app/server/chatbot/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, jsonify
+import openai
+from flask_cors import CORS  # For React communication with Flask
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+# API Key Set-Up
+openai.api_key = os.getenv("OPENAI_API_KEY")
+print("OPENAI_API_KEY:", openai.api_key)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template('chat.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    return get_Chat_response(input)
+
+
+def get_Chat_response(text):
+    
+    # Call model:
+    response = openai.chat.completions.create(
+        model="gpt-4o-mini",
+        store=True,
+        messages=[
+            {"role": "system", "content": "You are an expert in ER diagrams and database design and you are also called botER."},
+            {"role": "user", "content": text},
+        ],
+    )
+
+    # Getting the response text
+    return response.choices[0].message.content.strip()
+
+
+if __name__ == '__main__':
+    app.run()
--- zweckers-frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Main from './(tabs)/Main';
-
-test('renders learn react link', () => {
-  render(<Main />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
